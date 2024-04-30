3d2534ed3dca6f16c868949efa025f09a494210f
Yousef Hani <91964155+yousefhanni@users.noreply.github.com>
2024-04-30 15:32:49 +0300

    Add Categories Model

--- Bookify.Web/Data/ApplicationDbContext.cs
+++ Bookify.Web/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Bookify.Web.Core.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookify.Web.Data
@@ -9,5 +10,16 @@ namespace Bookify.Web.Data
 			: base(options)
 		{
 		}
-	}
+
+		//Write Fluent API To put Configs on Domain model and apply their configurations at DB Or create specific Configs classes and put line of apply configs
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+		 //builder.Entity<Category>().Property(e => e.CreatedOn).HasDefaultValueSql("GETDATE()");
+			base.OnModelCreating(builder); 
+		}
+
+        public DbSet<Category> Categories { get; set; }
+
+
+    }
 }
