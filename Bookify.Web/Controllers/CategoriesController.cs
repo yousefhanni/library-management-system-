570aa54de5c7877aff37ee640198312aade9d7e7
Yousef Hani <91964155+yousefhanni@users.noreply.github.com>
2024-05-02 00:41:20 +0300

    Added Create and Edit functionality to CategoriesController

--- Bookify.Web/Controllers/CategoriesController.cs
+++ Bookify.Web/Controllers/CategoriesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bookify.Web.Core.Models;
+using Bookify.Web.Core.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 namespace Bookify.Web.Controllers
 {
 	public class CategoriesController : Controller
@@ -10,11 +13,65 @@ namespace Bookify.Web.Controllers
 			_dbContext = dbContext;
 		}
 
-		public IActionResult Index()
-		{
-			//TODO: 1.use ViewModel
-			var categories = _dbContext.Categories.ToList();
-			return View(categories);
-		}
-	}
+        public IActionResult Index()
+        {
+            var categories = _dbContext.Categories.ToList();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("CreateEdit");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CreateEditCategoryViewModel model) 
+        {   
+            if (!ModelState.IsValid)//Server side validation
+                return View(model);
+
+             var category = new Category { Name = model.Name };//Manual Mapping
+            _dbContext.Add(category);
+             _dbContext.SaveChanges();    
+            
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var category = _dbContext.Categories.Find(id);
+            if (category == null)
+                return NotFound();
+
+             var viewModel=new CreateEditCategoryViewModel //Manual Mapping
+            {
+                Id = id,
+                Name = category.Name
+            };
+            return View("CreateEdit", viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Edit(CreateEditCategoryViewModel model)
+        {
+            if (!ModelState.IsValid)//Server side validation
+                return View("CreateEdit",model);
+
+            var category = _dbContext.Categories.Find(model.Id);
+             if (category == null)
+                return NotFound();
+ 
+             category.Name = model.Name;
+             category.LastUpdatedOn = DateTime.Now;
+
+            _dbContext.Update(category);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }   
 }
---
+++ Bookify.Web/Core/ViewModels/CreateEditCategoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Bookify.Web.Core.ViewModels
+{
+    public class CreateEditCategoryViewModel
+    {
+        public int Id { get; set; } //to case Edit
+
+        [MaxLength(100,ErrorMessage = "Max length cannot be more than 5 characters")]
+        public string Name { get; set; } = null!;
+    }
+}
